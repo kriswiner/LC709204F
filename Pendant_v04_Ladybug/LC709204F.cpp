686b0d1ea9a5393452fded1083203a53867ba46a
Kris Winer <kriswiner@users.noreply.github.com>
2021-10-08 15:54:20 -0700

    Update LC709204F.cpp

--- Pendant_v04_Ladybug/LC709204F.cpp
+++ Pendant_v04_Ladybug/LC709204F.cpp
@@ -19,151 +19,99 @@ LC709204F::LC709204F(I2Cdev* i2c_bus)
 
 uint16_t LC709204F::getChipID()
 {
-  uint8_t tmp[2] = {0, 0};
-  _i2c_bus->readBytes(LC709204F_ADDRESS, LC709204F_NUMBER_PARAM, 2, &tmp[0]); // read number of the parameter, should be 0x1001
-  uint16_t userid = (uint16_t) ((uint16_t)tmp[1] << 8) | tmp[0];
+  uint16_t userid = _i2c_bus->readBytes16(LC709204F_ADDRESS, LC709204F_NUMBER_PARAM); // read number of the parameter, should be 0x1001
   return userid;
 }
 
 
 void LC709204F::init()
 {
-  uint8_t tmp[2] = {0, 0};
-//  tmp[0] = 0x15;
-//  tmp[1] = 0x15;
-  tmp[0] = 0x28;
-  tmp[1] = 0x28;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_APA, 2, &tmp[0]);                 // set APA for 100 mAH capacity
-  tmp[0] = 0x00;
-  tmp[1] = 0x00;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_CHANGE_PARAM,  2, &tmp[0]);       // set change of parameter for 3.7 V 1S LiPo
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_STATUS_BIT,  2, &tmp[0]);         // set to I2C mode
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_APA, 0x1515);                 // set APA for 100 mAH capacity
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_CHANGE_PARAM,  0x0000);       // set change of parameter for 3.7 V 1S LiPo
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_STATUS_BIT,    0x0000);       // set to I2C mode
 
 // Set up interrupts
-  tmp[0] = 0xA0;
-  tmp[1] = 0x00;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_ALARM_LOW_RSOC,  2, &tmp[0]);     // set to alarm when RSOC falls below 10%
-  tmp[0] = 0x10;
-  tmp[1] = 0x0E;  //0x0E10 = 3600
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_ALARM_LOW_CELL_VLT,  2, &tmp[0]); // set to alarm when battery voltage falls below 3.6 V
-  tmp[0] = 0x3C;
-  tmp[1] = 0x0C;  //0x0C3C = 3132 = 2732 + 400 so alarms at 40 C
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_ALARM_HIGH_TEMP,  2, &tmp[0]);    // set to alarm when temperature rises above 40 C
-
-
-// Put into auto mode
-  tmp[0] = 0x00;
-  tmp[1] = 0x00;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_CURRENT_DIR,  2, &tmp[0]);        // set auto mode
-
-// Set empty cell voltage to 3.61 V
-  tmp[0] = 0x1A;
-  tmp[1] = 0x0E;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_EMPTY_CELL_VOLTAGE,  2, &tmp[0]); // set empty cell voltage
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_ALARM_LOW_RSOC,  0x00A0);     // set to alarm when RSOC falls below 10%
+  //0x0E10 = 3600
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_ALARM_LOW_CELL_VLT,  0x0E10); // set to alarm when battery voltage falls below 3.6 V
+  //0x0C3C = 3132 = 2732 + 400 so alarms at 40 C
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_ALARM_HIGH_TEMP,  0x0C3C);    // set to alarm when temperature rises above 40 C
 
 // Set power mode and clear battery status
-  tmp[0] = 0x01;
-  tmp[1] = 0x00;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_IC_POWERMODE,  2, &tmp[0]);       // set to operate mode
-  tmp[0] = 0x00;
-  tmp[1] = 0x00;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_BATTERY_STATUS,  2, &tmp[0]);     // reset battery status
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_IC_POWERMODE,  0x0001);       // set to operate mode
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_BATTERY_STATUS,  0x0000);     // reset battery status
 }
 
 
 void LC709204F::sleep()
 {
-  uint8_t tmp[2] = {0, 0};
-  tmp[0] = 0x02;
-  tmp[1] = 0x00;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_IC_POWERMODE,  2, &tmp[0]);       // set to sleep mode
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_IC_POWERMODE,  0x0002);      // set to sleep mode
 }
 
 
 void LC709204F::operate()
 {
-  uint8_t tmp[2] = {0, 0};
-  tmp[0] = 0x01;
-  tmp[1] = 0x00;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_IC_POWERMODE,  2, &tmp[0]);       // set to operate mode
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_IC_POWERMODE,  0x0001);       // set to operate mode
 }
+
 
 uint16_t LC709204F::getCellVoltage()
 {
-  uint8_t tmp[2] = {0, 0};
-  _i2c_bus->readBytes(LC709204F_ADDRESS, LC709204F_CELL_VOLTAGE, 2, &tmp[0]);
-  uint16_t volt = (uint16_t) ((uint16_t) tmp[1] << 8) | tmp[0];
+  uint16_t volt = _i2c_bus->readBytes16(LC709204F_ADDRESS, LC709204F_CELL_VOLTAGE);
   return volt;
 }
 
 
 uint16_t LC709204F::getRSOC()
 {
-  uint8_t tmp[2] = {0, 0};
-  _i2c_bus->readBytes(LC709204F_ADDRESS, LC709204F_RSOC, 2, &tmp[0]);
-  uint16_t rsoc = (uint16_t) ((uint16_t) tmp[1] << 8) | tmp[0];
+  uint16_t rsoc = _i2c_bus->readBytes16(LC709204F_ADDRESS, LC709204F_RSOC);
   return rsoc;
 }
 
 
 uint16_t LC709204F::getITE()
 {
-  uint8_t tmp[2] = {0, 0};
-  _i2c_bus->readBytes(LC709204F_ADDRESS, LC709204F_ITE, 2, &tmp[0]);
-  uint16_t ite = (uint16_t) ((uint16_t) tmp[1] << 8) | tmp[0];
+  uint16_t ite = _i2c_bus->readBytes16(LC709204F_ADDRESS, LC709204F_ITE);
   return ite;
 }
 
 
 uint16_t LC709204F::getStatus()
 {
-  uint8_t tmp[2] = {0, 0};
-  _i2c_bus->readBytes(LC709204F_ADDRESS, LC709204F_BATTERY_STATUS, 2, &tmp[0]);
-  uint16_t stat = (uint16_t) ((uint16_t) tmp[1] << 8) | tmp[0];
+  uint16_t stat = _i2c_bus->readBytes16(LC709204F_ADDRESS, LC709204F_BATTERY_STATUS);
   return stat;
 }
 
 
 void LC709204F::clearStatus()
 {
-  uint8_t tmp[2] = {0, 0};
-  tmp[0] = 0x00;
-  tmp[1] = 0x00;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_BATTERY_STATUS,  2, &tmp[0]);     // reset battery status
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_BATTERY_STATUS,  0x0000);     // reset battery status
 }
 
 
 uint16_t LC709204F::timetoEmpty()
 {
-  uint8_t tmp[2] = {0, 0};
-  _i2c_bus->readBytes(LC709204F_ADDRESS, LC709204F_TIME_TO_EMPTY, 2, &tmp[0]);
-  uint16_t Etime = (uint16_t) ((uint16_t)tmp[1] << 8) | tmp[0];
-  return Etime;
+  uint16_t time = _i2c_bus->readBytes16(LC709204F_ADDRESS, LC709204F_TIME_TO_EMPTY);
+  return time;
 }
 
 
 uint16_t LC709204F::stateofHealth()
 {
-  uint8_t tmp[2] = {0, 0};
-  _i2c_bus->readBytes(LC709204F_ADDRESS, LC709204F_STATE_OF_HEALTH, 2, &tmp[0]);
-  uint16_t health = (uint16_t) ((uint16_t)tmp[1] << 8) | tmp[0];
+  uint16_t health = _i2c_bus->readBytes16(LC709204F_ADDRESS, LC709204F_STATE_OF_HEALTH);
   return health;
 }
 
 
 void LC709204F::setTemperature(uint16_t temperature)
 {
-  uint8_t tmp[2] = {0, 0};
-  tmp[0] = temperature & 0x00FF;
-  tmp[1] = (temperature & 0xFF00) >> 8;
-  _i2c_bus->writeBytes(LC709204F_ADDRESS, LC709204F_CELL_TEMP, 2, &tmp[0]); // input cell (MCU) temperature in 0.1 K
+  _i2c_bus->writeBytes16(LC709204F_ADDRESS, LC709204F_CELL_TEMP, temperature); // input cell (MCU) temperature in 0.1 K
 }
 
 
 uint16_t LC709204F::getTemperature()
 {
-  uint8_t tmp[2] = {0, 0};
-  _i2c_bus->readBytes(LC709204F_ADDRESS, LC709204F_CELL_TEMP, 2, &tmp[0]);
-  uint16_t cell = (uint16_t) ((uint16_t)tmp[1] << 8) | tmp[0];
+  uint16_t cell = _i2c_bus->readBytes16(LC709204F_ADDRESS, LC709204F_CELL_TEMP);
   return cell;
 }
+
